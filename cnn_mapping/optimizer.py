1be33c5dc10fbb53e48b65034b243ed630d6e197
xuanyoya <xuany@stanford.edu>
2019-01-14 12:05:13 -0800

    [optimizer]

--- cnn_mapping/optimizer.py
+++ cnn_mapping/optimizer.py
@@ -12,6 +12,7 @@ def opt_optimizer(resource, layer, hint=None, verbose=False):
     Evaluate the cost of each mapping point,
     record the mapping_point with the smallest cost
     '''
+    valid = True
     if hint is not None and hint.partition_loops is None:
         valid = cost_model.valid_dataflow(resource, hint.schedule_hint)
     assert valid == True, "Specified schedule doesn't satisfy the utilization threshold, please check partitioning_size"
